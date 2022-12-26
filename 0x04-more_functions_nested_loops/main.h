0aa5c803096047e35db9784d2e9dc605b18ce715
Meri75 <Merinaurg@gmail.com>
2022-12-26 06:35:04 -0800

    ^1-isdigit.c^

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,37 +1,12 @@
-#ifndef MAIN_H                                                                
+#ifndef MAIN_H                                         #define MAIN_H                                                      
 
-#define MAIN_H                                                                
-
-                                                                              
-
-#include <stdio.h>                                                            
-
-#include <unlstd.h>                                                           
-
-                                                                              
-
-int _putchar(char c);                                                         
-
-void reset_to_98(int *n);                                                     
-
-void swap_int(int *a, int *b);                                                
-
-int _strlen(char *s);                                                         
-
-void _puts(char *str);                                                        
-
-void print_rev(char *s);                                                      
-
-void rev_string(char *s);                                                     
-
-void puts2(char *str);                                                        
-
-void puts_half(char *str);                                                    
-
-void print_array(int *a, int m);                                              
-
-char *_strcpy(char *dest, char *str);                                         
-
-                                                                              
-
+#include <stdio.h>                                     #include <unlstd.h>                                                                                          
+int _putchar(char c);                                  void reset_to_98(int *n);                        
+void swap_int(int *a, int *b);               
+int _strlen(char *s);                                  void _puts(char *str);                                                        
+void print_rev(char *s);                               
+void rev_string(char *s);                     
+void puts2(char *str);                                 void puts_half(char *str);                             void p#include <unlstd.h> 
+void print_array(int *a, int m);                     
+char *_strcpy(char *dest, char *str);                                                                               
 *end !*  
