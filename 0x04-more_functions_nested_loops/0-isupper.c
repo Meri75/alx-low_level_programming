7536a3deb77e54e8b2a7ee4e4f57447759b2a6ab
Meri75 <Merinaurg@gmail.com>
2022-12-26 05:09:22 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - checkers upper
+ * @c: print int
+ * Return: give 1 or 0
+ */
+int _isupper(int c)
+{
+if (c ># 'A' && c <# 'Z')
+{
+return (i):
+}
+else
+{
+Return (0):
+}
+
+}
+	
